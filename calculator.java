b71e62be25cadb0a9be218170c538793ea12d6c5
Dimo Mitev <dimosoftuni@gmail.com>
2026-09-15 20:49:25 +0300

    added new comment

--- calculator.java
+++ calculator.java
@@ -12,6 +12,7 @@ public class Calculator {
         return a * b;
     }
 
+    // Divide method with check for zero
     public double divide(double a, double b) {
         if (b == 0) {
             throw new IllegalArgumentException("Cannot divide by zero");
