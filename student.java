eb57948ccd25cdfd284cc5c04c3a9a89f2471828
Dimo Mitev <dimosoftuni@gmail.com>
2026-09-16 19:30:50 +0300

    add new class for student

---
+++ student.java
@@ -0,0 +1,26 @@
+public class Calculator {
+
+    // Add method ot DIMO lokalno
+    public int add(int a, int b int c) {
+        return a + b + c;
+    }
+
+    // Subtract method
+    public double subtract(double a, double b) {
+        return a - b;
+    }
+
+    // Promqna direktno na servera
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    // Divide method with check for zero promqna ot Gosho
+    public double divide(double a, double b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Cannot divide by zero");
+        }
+
+        return a / b;
+    }
+}
